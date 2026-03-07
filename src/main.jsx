b08baedbfdba600fd9f4a5b45a77cb8823d6c35f
sundar <sundars@berkeley.edu>
2026-03-07 15:03:41 -0800

    ui

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import LeitmotifApp from "../mockup.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LeitmotifApp />
+  </React.StrictMode>
+);
